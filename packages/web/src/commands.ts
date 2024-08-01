25a24738ca08f18c3671d604e2fb6b219e58bce0
NISHIZAWA Shuntaro <me@s2n.tech>
2024-08-02 01:20:41 +0900

    refactor: コマンドを登録処理を移動

--- packages/web/src/commands.ts
+++ packages/web/src/commands.ts
@@ -13,7 +13,7 @@ export const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
 			{
 				type: ApplicationCommandOptionType.SubcommandGroup,
 				name: "register",
-				description: "学生証やSuicaを登録します。",
+				description: "学生証を登録します。",
 				options: [
 					{
 						name: "student-card",
@@ -24,20 +24,6 @@ export const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
 								name: "student-id",
 								description: "学籍番号",
 								type: ApplicationCommandOptionType.Integer,
-								required: true,
-							},
-						],
-					},
-					{
-						name: "suica",
-						description:
-							"Suicaを登録します。Suica以外の交通系ICカードも登録できる(かもしれない)。",
-						type: ApplicationCommandOptionType.Subcommand,
-						options: [
-							{
-								name: "idm",
-								description: "idm",
-								type: ApplicationCommandOptionType.String,
 								required: true,
 							},
 						],
