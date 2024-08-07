db32a9d087680aae9d999ffc1056bbb50587e9f7
NISHIZAWA Shuntaro <me@s2n.tech>
2024-08-07 23:54:23 +0900

    refactor: 不要なパッケージの削除

--- packages/app/src/handlers/discord/index.ts
+++
@@ -1,9 +0,0 @@
-import type { ChatInputCommandInteraction } from "discord.js";
-
-import { ping } from "./ping";
-
-export type CommandHandler = (
-	interaction: ChatInputCommandInteraction,
-) => Promise<void>;
-
-export const handlers = new Map<string, CommandHandler>([["ping", ping]]);
--- packages/app/src/handlers/discord/ping.ts
+++
@@ -1,9 +0,0 @@
-import type { CommandHandler } from ".";
-
-export const ping: CommandHandler = async (interaction) => {
-	const diff = Date.now() - interaction.createdTimestamp;
-
-	await interaction.reply({
-		content: `Pong! \`${diff}ms\``,
-	});
-};
