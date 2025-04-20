7f56717d74ec5e555db50b07012fd6d8db01d27f
NISHIZAWA Shuntaro <me@s2n.tech>
2025-04-20 12:08:33 +0900

    feat: コマンドを追加

--- packages/api/scripts/register.ts
+++ packages/api/scripts/register.ts
@@ -1,13 +1,19 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 
-import { commands } from "@/commands";
+import { pingCommand, roomAdminCommand, roomCommand } from "@/commands";
 
 const token = process.env["DISCORD_BOT_TOKEN"]!;
 const applicationId = process.env["DISCORD_APPLICATION_ID"]!;
 const guildId = process.env["DISCORD_GUILD_ID"]!;
 
 const rest = new REST({ version: "10" }).setToken(token);
+
+const commands = [
+	pingCommand.toJSON(),
+	roomCommand.toJSON(),
+	roomAdminCommand.toJSON(),
+];
 
 await rest.put(Routes.applicationGuildCommands(applicationId, guildId), {
 	body: commands,
--- packages/api/src/commands.ts
+++ packages/api/src/commands.ts
@@ -1,35 +1,62 @@
-import type { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
-import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import { SlashCommandBuilder } from "@discordjs/builders";
 
-export const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
-	{
-		name: "ping",
-		description: "pingをBotに送ります。",
-	},
-	{
-		name: "room-manager",
-		description: "部室の管理を行います。",
-		options: [
-			{
-				type: ApplicationCommandOptionType.SubcommandGroup,
-				name: "register",
-				description: "学生証を登録します。",
-				options: [
-					{
-						name: "student-card",
-						description: "学生証を登録します。",
-						type: ApplicationCommandOptionType.Subcommand,
-						options: [
-							{
-								name: "student-id",
-								description: "学籍番号",
-								type: ApplicationCommandOptionType.Integer,
-								required: true,
-							},
-						],
-					},
-				],
-			},
-		],
-	},
-];
+export const pingCommand = new SlashCommandBuilder()
+	.setName("ping")
+	.setDescription("pingをBotに送ります。");
+
+export const roomCommand = new SlashCommandBuilder()
+	.setName("room")
+	.setDescription("入退出システムを操作します。")
+	.addSubcommandGroup((group) =>
+		group
+			.setName("register")
+			.setDescription("学生証またはSuicaを登録します。")
+			.addSubcommand((subcommand) =>
+				subcommand
+					.setName("student-card")
+					.setDescription("学生証を登録します。")
+					.addIntegerOption((option) =>
+						option
+							.setName("student-id")
+							.setNameLocalization("ja", "学籍番号")
+							.setDescription("学籍番号")
+							.setRequired(true),
+					),
+			)
+			.addSubcommand((subcommand) =>
+				subcommand
+					.setName("suica")
+					.setDescription("Suicaを登録します。")
+					.addStringOption((option) =>
+						option
+							.setName("idm")
+							.setDescription("SuicaのIDm")
+							.setRequired(true),
+					),
+			),
+	)
+	.addSubcommand((subcommand) =>
+		subcommand.setName("list").setDescription("現在部室にいる人を表示します。"),
+	);
+
+export const roomAdminCommand = new SlashCommandBuilder()
+	.setName("room-admin")
+	.setDescription("入退出システムの管理を行います。")
+	.addSubcommandGroup((group) =>
+		group
+			.setName("setting")
+			.setDescription(
+				"設定を操作します。設定値を省略すると現在の値を表示します。",
+			)
+			.addSubcommand((subcommand) =>
+				subcommand
+					.setName("register")
+					.setDescription("新規登録の許可")
+					.addBooleanOption((option) =>
+						option
+							.setName("allow")
+							.setNameLocalization("ja", "許可")
+							.setDescription("登録を許可するかどうか"),
+					),
+			),
+	);
