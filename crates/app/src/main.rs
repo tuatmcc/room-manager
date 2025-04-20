4be13c17e436a92ca1bd38ed6c12d3859c68c045
NISHIZAWA Shuntaro <me@s2n.tech>
2025-04-21 00:49:30 +0900

    refactor: カードごとの処理を共通化

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -2,10 +2,8 @@ mod reader;
 
 use std::io::Cursor;
 
-use reader::{
-    open_reader, scan_student_card, scan_suica_card, wait_for_student_card_release,
-    wait_for_suica_card_release,
-};
+use reader::{open_reader, scan_card, wait_for_release};
+use reqwest::Client;
 use rodio::{Decoder, OutputStream, Sink};
 use serde::{Deserialize, Serialize};
 use tracing::info;
@@ -32,6 +30,11 @@ enum TouchCardResponse {
     Error { error: String },
 }
 
+pub enum CardKind {
+    Student(u32),
+    Suica(String),
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
@@ -42,71 +45,49 @@ async fn main() -> anyhow::Result<()> {
     let sink = Sink::try_new(&stream_handle)?;
 
     loop {
-        if let Some((card, student_id)) = scan_student_card(&mut reader)? {
-            info!("Student card detected: {:?}", student_id);
-
-            let res = client
-                .post("https://dev.s2n.tech/local-device/touch-card")
-                .json(&TouchCardRequest {
-                    student_id: Some(student_id),
-                    suica_idm: None,
-                })
-                .send()
-                .await?
-                .json::<TouchCardResponse>()
-                .await?;
-
-            match res {
-                TouchCardResponse::Success { status } => {
-                    info!("Student card touched successfully: {:?}", status);
-                    let buf = match status {
-                        RoomEntryStatus::Entry => include_bytes!("./sounds/hello.wav").as_slice(),
-                        RoomEntryStatus::Exit => include_bytes!("./sounds/goodbye.wav").as_slice(),
-                    };
-                    let reader = Cursor::new(buf);
-                    let source = Decoder::new(reader)?;
-                    sink.append(source);
-                    info!("Playing sound");
-                }
-                TouchCardResponse::Error { error } => {
-                    info!("Error touching student card: {:?}", error);
-                }
-            }
-
-            wait_for_student_card_release(&mut reader, &card)?;
+        let Some(detected) = scan_card(&mut reader)? else {
+            continue;
         };
-        if let Some((card, suica_idm)) = scan_suica_card(&mut reader)? {
-            info!("Suica card detected: {:?}", suica_idm);
 
-            let res = client
-                .post("https://dev.s2n.tech/local-device/touch-card")
-                .json(&TouchCardRequest {
-                    student_id: None,
-                    suica_idm: Some(suica_idm),
-                })
-                .send()
-                .await?
-                .json::<TouchCardResponse>()
-                .await?;
+        handle_card(&client, &sink, &detected.kind).await?;
 
-            match res {
-                TouchCardResponse::Success { status } => {
-                    info!("Student card touched successfully: {:?}", status);
-                    let buf = match status {
-                        RoomEntryStatus::Entry => include_bytes!("./sounds/hello.wav").as_slice(),
-                        RoomEntryStatus::Exit => include_bytes!("./sounds/goodbye.wav").as_slice(),
-                    };
-                    let reader = Cursor::new(buf);
-                    let source = Decoder::new(reader)?;
-                    sink.append(source);
-                    info!("Playing sound");
-                }
-                TouchCardResponse::Error { error } => {
-                    info!("Error touching student card: {:?}", error);
-                }
-            }
-
-            wait_for_suica_card_release(&mut reader, &card)?;
-        }
+        wait_for_release(&mut reader, &detected.card, &detected.kind)?;
     }
+}
+
+async fn handle_card(client: &Client, sink: &Sink, kind: &CardKind) -> anyhow::Result<()> {
+    let req = match kind {
+        CardKind::Student(student_id) => TouchCardRequest {
+            student_id: Some(*student_id),
+            suica_idm: None,
+        },
+        CardKind::Suica(suica_idm) => TouchCardRequest {
+            student_id: None,
+            suica_idm: Some(suica_idm.clone()),
+        },
+    };
+
+    let res = client
+        .post("https://dev.s2n.tech/local-device/touch-card")
+        .json(&req)
+        .send()
+        .await?
+        .json::<TouchCardResponse>()
+        .await?;
+
+    if let TouchCardResponse::Success { status } = res {
+        info!("Card touched successfully: {:?}", status);
+        let buf = match status {
+            RoomEntryStatus::Entry => include_bytes!("./sounds/hello.wav").as_slice(),
+            RoomEntryStatus::Exit => include_bytes!("./sounds/goodbye.wav").as_slice(),
+        };
+        let reader = Cursor::new(buf);
+        let source = Decoder::new(reader)?;
+        sink.append(source);
+        info!("Playing sound");
+    } else {
+        info!("Error touching card: {:?}", res);
+    }
+
+    Ok(())
 }
--- crates/app/src/reader.rs
+++ crates/app/src/reader.rs
@@ -5,6 +5,8 @@ use pasori::{
 };
 use tracing::info;
 
+use crate::CardKind;
+
 const VENDER_ID: u16 = 0x054c;
 const PRODUCT_ID: u16 = 0x06c3;
 
@@ -15,13 +17,36 @@ const SUICA_SERVICE_CODE: u16 = 0x090f; // 残高が格納されているサー�
 
 type Reader = Box<dyn Device + Send + Sync>;
 
+pub struct DetectedCard {
+    pub card: Card,
+    pub kind: CardKind,
+}
+
 pub fn open_reader() -> anyhow::Result<Reader> {
     let transport = Usb::from_id(VENDER_ID, PRODUCT_ID)?;
     let device = RCS380::new(transport)?;
     Ok(Box::new(device))
 }
 
-pub fn scan_student_card(reader: &mut Reader) -> anyhow::Result<Option<(Card, u32)>> {
+pub fn scan_card(reader: &mut Reader) -> anyhow::Result<Option<DetectedCard>> {
+    if let Some((card, student_id)) = scan_student_card(reader)? {
+        return Ok(Some(DetectedCard {
+            card,
+            kind: CardKind::Student(student_id),
+        }));
+    }
+
+    if let Some((card, idm)) = scan_suica_card(reader)? {
+        return Ok(Some(DetectedCard {
+            card,
+            kind: CardKind::Suica(idm),
+        }));
+    }
+
+    Ok(None)
+}
+
+fn scan_student_card(reader: &mut Reader) -> anyhow::Result<Option<(Card, u32)>> {
     let Ok(polling_res) = reader.polling(
         pasori::device::Bitrate::Bitrate424kbs,
         Some(STUDENT_CARD_SYSTEM_CODE),
@@ -53,7 +78,7 @@ pub fn scan_student_card(reader: &mut Reader) -> anyhow::Result<Option<(Card, u3
     Ok(Some((card, student_id)))
 }
 
-pub fn scan_suica_card(reader: &mut Reader) -> anyhow::Result<Option<(Card, String)>> {
+fn scan_suica_card(reader: &mut Reader) -> anyhow::Result<Option<(Card, String)>> {
     let Ok(polling_res) = reader.polling(
         pasori::device::Bitrate::Bitrate424kbs,
         Some(SUICA_SYSTEM_CODE),
@@ -87,31 +112,14 @@ pub fn scan_suica_card(reader: &mut Reader) -> anyhow::Result<Option<(Card, Stri
     Ok(Some((card, idm)))
 }
 
-pub fn wait_for_student_card_release(reader: &mut Reader, card: &Card) -> anyhow::Result<()> {
+pub fn wait_for_release(reader: &mut Reader, card: &Card, kind: &CardKind) -> anyhow::Result<()> {
     loop {
         let Ok(polling_res) = reader.polling(
             pasori::device::Bitrate::Bitrate424kbs,
-            Some(STUDENT_CARD_SYSTEM_CODE),
-            pasori::felica::PollingRequestCode::SystemCode,
-            pasori::felica::PollingTimeSlot::Slot0,
-        ) else {
-            info!("Card released: {:?}", idm_to_string(&card.idm()));
-            return Ok(());
-        };
-
-        let new_card = polling_res.card;
-        if new_card.idm() != card.idm() {
-            info!("Card released: {:?}", idm_to_string(&new_card.idm()));
-            return Ok(());
-        }
-    }
-}
-
-pub fn wait_for_suica_card_release(reader: &mut Reader, card: &Card) -> anyhow::Result<()> {
-    loop {
-        let Ok(polling_res) = reader.polling(
-            pasori::device::Bitrate::Bitrate424kbs,
-            Some(SUICA_SYSTEM_CODE),
+            Some(match kind {
+                CardKind::Student(_) => STUDENT_CARD_SYSTEM_CODE,
+                CardKind::Suica(_) => SUICA_SYSTEM_CODE,
+            }),
             pasori::felica::PollingRequestCode::SystemCode,
             pasori::felica::PollingTimeSlot::Slot0,
         ) else {
