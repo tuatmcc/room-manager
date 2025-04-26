94f13314f2ca71a1497be9b917b20c2f1da9bec8
NISHIZAWA Shuntaro <me@s2n.tech>
2025-04-26 18:34:17 +0900

    feat: ローカルデバイスでもAPIに認証ヘッダーを追加

---
+++ crates/app/src/config.rs
@@ -0,0 +1,10 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+pub struct Config {
+    #[clap(long, env, hide_env_values = true)]
+    pub api_path: String,
+
+    #[clap(long, env, hide_env_values = true)]
+    pub api_token: String,
+}
