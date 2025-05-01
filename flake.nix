c9c70cf382d657591e7bdafc041416f52017512b
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-01 16:36:40 +0900

    feat: nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A basic flake to with flake-parts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ self, systems, nixpkgs, flake-parts, rust-overlay, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem = { config, pkgs, system, ... }: {
+        # Add rust-overlay to the package set
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
+
+        # When execute `nix develop`, you go in shell installed nil.
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            pnpm
+            # Add rust toolchain based on mise.toml configuration
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+          ];
+        };
+      };
+    };
+}
