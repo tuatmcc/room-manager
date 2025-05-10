d36b4893ec903634d772531013c986871dc9557b
NISHIZAWA Shuntaro <me@s2n.tech>
2025-05-11 02:39:48 +0900

    chore: vitestを追加

---
+++ packages/api/vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		// Vitest の設定オプションをここに記述します
+		// 例:
+		// globals: true,
+		// environment: 'node',
+	},
+});
